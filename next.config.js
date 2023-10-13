4f571363590f33e56239464328f89a8edca728ae
Liaksej <tarusov.liaksej@gmail.com>
2023-10-13 23:18:41 +0200

    Remove unused CSS file, update .gitignore and config files
    Deleted 'page.module.css' as it's not being used, preventing unused code. Updated '.gitignore' to ignore IDE related folders, preventing unnecessary commits and start managing environment-related paths on 'next.config.js'. Updated 'README.md', removed default Next.js content and populated with project-specific information, making it more informative for developers.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/ra-hoc-time" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
